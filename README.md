6f629d86e2dc72a04e34c813ce378b8a5c1b3dbf
Laszlo Agocs <laszlo.agocs@qt.io>
2018-08-30 15:48:43 +0200

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+Experiments for a Vulkan renderer in Qt 6. Nothing to see here yet.
+
+The test apps (under tests/manual) rely on https://github.com/alpqr/qtshaderstack17 for shader compilation.
---
+++ tests/manual/vulkanwindow/main.cpp
@@ -0,0 +1,77 @@
+/****************************************************************************
+ **
+ ** Copyright (C) 2018 The Qt Company Ltd.
+ ** Contact: https://www.qt.io/licensing/
+ **
+ ** This file is part of the test suite of the Qt Toolkit.
+ **
+ ** $QT_BEGIN_LICENSE:GPL-EXCEPT$
+ ** Commercial License Usage
+ ** Licensees holding valid commercial Qt licenses may use this file in
+ ** accordance with the commercial license agreement provided with the
+ ** Software or, alternatively, in accordance with the terms contained in
+ ** a written agreement between you and The Qt Company. For licensing terms
+ ** and conditions see https://www.qt.io/terms-conditions. For further
+ ** information use the contact form at https://www.qt.io/contact-us.
+ **
+ ** GNU General Public License Usage
+ ** Alternatively, this file may be used under the terms of the GNU
+ ** General Public License version 3 as published by the Free Software
+ ** Foundation with exceptions as appearing in the file LICENSE.GPL3-EXCEPT
+ ** included in the packaging of this file. Please review the following
+ ** information to ensure the GNU General Public License requirements will
+ ** be met: https://www.gnu.org/licenses/gpl-3.0.html.
+ **
+ ** $QT_END_LICENSE$
+ **
+ ****************************************************************************/
+
+#include <QGuiApplication>
+#include <QVulkanWindow>
+#include <QLoggingCategory>
+
+class VulkanWindow : public QVulkanWindow
+{
+public:
+    QVulkanWindowRenderer *createRenderer() override;
+};
+
+QVulkanWindowRenderer *VulkanWindow::createRenderer()
+{
+    return nullptr;
+}
+
+int main(int argc, char **argv)
+{
+    QGuiApplication app(argc, argv);
+
+    QLoggingCategory::setFilterRules(QStringLiteral("qt.vulkan=true"));
+
+    QVulkanInstance inst;
+
+#ifndef Q_OS_ANDROID
+    inst.setLayers(QByteArrayList() << "VK_LAYER_LUNARG_standard_validation");
+#else
+    inst.setLayers(QByteArrayList()
+                   << "VK_LAYER_GOOGLE_threading"
+                   << "VK_LAYER_LUNARG_parameter_validation"
+                   << "VK_LAYER_LUNARG_object_tracker"
+                   << "VK_LAYER_LUNARG_core_validation"
+                   << "VK_LAYER_LUNARG_image"
+                   << "VK_LAYER_LUNARG_swapchain"
+                   << "VK_LAYER_GOOGLE_unique_objects");
+#endif
+
+    VulkanWindow vkw;
+    if (inst.create()) {
+        vkw.setVulkanInstance(&inst);
+        vkw.resize(1280, 720);
+        vkw.setTitle(QLatin1String("Vulkan"));
+        vkw.show();
+    } else {
+        qWarning("Vulkan not supported");
+        return 1;
+    }
+
+    return app.exec();
+}
